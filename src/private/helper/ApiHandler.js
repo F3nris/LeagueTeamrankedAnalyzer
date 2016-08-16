32b3029baab3053c21605983e6d8f131106342d0
Michael Thiele-Maas <m.thielemaas@gmail.com>
2016-08-16 13:56:37 +0200

    Small bugfix and cleaned up code (teams by summoner now takes and id instead of chaining two requests, this allows to use a stored summonerid (and therefore query one request less))

--- src/private/helper/ApiHandler.js
+++ src/private/helper/ApiHandler.js
@@ -7,34 +7,25 @@ var ApiHandler = module.exports = function (name) {
   //this.analyze();
 }
 
-ApiHandler.prototype.getTeamsBySummoner = function(summonerName, region) {
+ApiHandler.prototype.getTeamsBySummonerID = function(summonerID, region) {
   var self = this;
+  region = region.toLowerCase();
 
   return new Promise(function (resolve, reject) {
-    summonerName = summonerName.toLowerCase();
-    region = region.toLowerCase();
-
-    var summonerPromise = self.getSummoner(summonerName, region);
-
-    summonerPromise.then(function(summonerData){
-      var summonerID = summonerData.id;
-
-      rp(apiConfig.API_BASE_URL + region + "/v2.4/team/by-summoner/"+summonerID+"?api_key="+apiConfig.API_KEY)
-      .then(function (jsonAsString) {
-        var dataArray = JSON.parse(jsonAsString)[summonerID];
-        var result = dataArray.map(function(el) {
-          return {
-            "id": el.fullId,
-            "name": el.name
-          };
-        });
-
-        resolve(result);
-      })
-      .catch(function (err) {
-        reject(err);
+    rp(apiConfig.API_BASE_URL + region + "/v2.4/team/by-summoner/"+summonerID+"?api_key="+apiConfig.API_KEY)
+    .then(function (jsonAsString) {
+      var dataArray = JSON.parse(jsonAsString)[summonerID];
+      var result = dataArray.map(function(el) {
+        return {
+          "id": el.fullId,
+          "name": el.name
+        };
       });
-    }).catch(self.errorFunction);
+      resolve(result);
+    })
+    .catch(function (err) {
+      reject(err);
+    });
   });
 }
 
